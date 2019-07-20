4f99880a96f350e7704dbf15836b3a0c4106dc80
neingeist <neingeist@bl0rg.net>
2019-07-20 22:01:20 +0200

    💄 Remove weird space before ()

--- no-counters.py
+++ no-counters.py
@@ -1,19 +1,19 @@
 from gi.repository import GObject, Liferea
 
-class NoCountersPlugin (GObject.Object, Liferea.ShellActivatable):
+class NoCountersPlugin(GObject.Object, Liferea.ShellActivatable):
     __gtype_name__ = 'NoCountersPlugin'
 
-    shell = GObject.property (type=Liferea.Shell)
+    shell = GObject.property(type=Liferea.Shell)
 
-    def do_activate (self):
-        self.treeview = self.shell.lookup ("feedlist")
-        self.counter_column = self.treeview.get_column (1)
+    def do_activate(self):
+        self.treeview = self.shell.lookup("feedlist")
+        self.counter_column = self.treeview.get_column(1)
 
-        self.counter_column.set_visible (False)
-        self.treeview.queue_draw ()
+        self.counter_column.set_visible(False)
+        self.treeview.queue_draw()
         return
 
-    def do_deactivate (self):
-        self.counter_column.set_visible (True)
-        self.treeview.queue_draw ()
+    def do_deactivate(self):
+        self.counter_column.set_visible(True)
+        self.treeview.queue_draw()
         return
